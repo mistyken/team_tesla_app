ed726341efafc2fe9d4a5169818072691ed4fd36
Kenneth Chen <mistyken@gmail.com>
2020-07-10 22:06:16 -0700

    click button test:

--- app/src/main/java/com/example/cmpe235teslaapp/MainActivity.kt
+++ app/src/main/java/com/example/cmpe235teslaapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.cmpe235teslaapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    var txtString: TextView? = null
+    var url = "https://reqres.in/api/users/2"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun login(view: View) {
+        Toast.makeText(this, "login pressed", Toast.LENGTH_SHORT).show()
+
     }
 }
