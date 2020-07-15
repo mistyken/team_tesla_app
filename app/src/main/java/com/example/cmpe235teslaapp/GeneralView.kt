51eb9b042c6e85e2f0cd0c47655ca60b0e839333
Kenneth Chen <mistyken@gmail.com>
2020-07-15 13:01:53 -0700

    wip commit for 2nd view

---
+++ app/src/main/java/com/example/cmpe235teslaapp/GeneralView.kt
@@ -0,0 +1,11 @@
+package com.example.cmpe235teslaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GeneralView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_general_view)
+    }
+}
